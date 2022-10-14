7fa1b7dfd9d78c2263e3866251bece0d2584df97
Julian Psotta <julianpsotta@gmail.com>
2022-10-14 14:03:01 +0200

    add exceptions

---
+++ gtfs_general/exceptions/extractor_exceptions.py
@@ -0,0 +1,28 @@
+from gtfs_general import logger
+
+
+class CustomException(Exception):
+    """Base custom exception class"""
+
+    pass
+
+
+class GtfsIncompleteException(CustomException):
+    def __init__(self) -> None:
+        self.message = "Your GTFS input is missing required files."
+        logger.error(self.message)
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class GtfsFileNotFound(CustomException):
+    def __init__(self, file_path: str) -> None:
+        self.message = f"Couldn't find the given file: {file_path}"
+        self.file_path = file_path
+        logger.error(self.message)
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return f"{self.message}: {self.file_path}"
