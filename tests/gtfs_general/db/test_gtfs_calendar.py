32f8772ee5645a9a61f48839bb839b476f76ea5b
Julian Psotta <julianpsotta@gmail.com>
2022-11-12 20:54:57 +0100

    use calendar_factory

--- tests/gtfs_general/db/test_gtfs_calendar.py
+++ tests/gtfs_general/db/test_gtfs_calendar.py
@@ -5,129 +5,54 @@ from sqlalchemy.exc import IntegrityError, PendingRollbackError
 from sqlmodel import Session, select
 
 from gtfs_general.db import Calendar
-from gtfs_general.db.gtfs import DepartsOnDay
+from gtfs_general.db.factories import calendar_factory
 
 
 @pytest.mark.parametrize(
-    "service_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday,start_date,end_date,expect_to_fail",
+    "service_id,start_date,end_date,expect_to_fail",
     [
         (
             None,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
             datetime.datetime.now(),
             datetime.datetime.now() + datetime.timedelta(days=8),
             True,
         ),
         (
             "1",
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
             datetime.datetime.utcnow(),
             datetime.datetime.utcnow() + datetime.timedelta(days=8),
             False,
         ),
         (
             "2",
-            None,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
             datetime.datetime.now(),
             datetime.datetime.now() + datetime.timedelta(days=8),
-            True,
-        ),
-        (
-            "3",
-            123,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            datetime.datetime.now(),
-            datetime.datetime.now() + datetime.timedelta(days=8),
-            True,
+            False,
         ),
         (
             "4",
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
             None,
             datetime.datetime.now() + datetime.timedelta(days=8),
             True,
         ),
         (
             "6",
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
             datetime.datetime.now(),
             None,
             True,
-        ),
-        (
-            "7",
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            DepartsOnDay.DEPARTING,
-            DepartsOnDay.NOT_DEPARTING,
-            datetime.datetime.now(),
-            datetime.datetime.now() + datetime.timedelta(days=8),
-            False,
         ),
     ],
 )
 def test_calendar(
     service_id: str,
-    monday: DepartsOnDay,
-    tuesday: int,
-    wednesday: int,
-    thursday: int,
-    friday: int,
-    saturday: int,
-    sunday: int,
-    start_date: int,
-    end_date: int,
+    start_date: datetime.datetime,
+    end_date: datetime.datetime,
     expect_to_fail: bool,
     in_memory_spatialite_session: Session,
 ) -> None:
     session: Session = in_memory_spatialite_session
-    original_object = Calendar(
+    original_object = calendar_factory(
         service_id=service_id,
-        monday=monday,
-        tuesday=tuesday,
-        wednesday=wednesday,
-        thursday=thursday,
-        friday=friday,
-        saturday=saturday,
-        sunday=sunday,
         start_date=start_date,
         end_date=end_date,
     )
@@ -147,29 +72,17 @@ def test_calendar_duplicate_fail(
     in_memory_spatialite_session: Session,
 ) -> None:
     session: Session = in_memory_spatialite_session
-    first_object = Calendar(
+    start_date = datetime.datetime.now()
+    end_date = datetime.datetime.now() + datetime.timedelta(days=8)
+    first_object = calendar_factory(
         service_id="1",
-        monday=DepartsOnDay.NOT_DEPARTING,
-        tuesday=DepartsOnDay.NOT_DEPARTING,
-        wednesday=DepartsOnDay.NOT_DEPARTING,
-        thursday=DepartsOnDay.NOT_DEPARTING,
-        friday=DepartsOnDay.NOT_DEPARTING,
-        saturday=DepartsOnDay.NOT_DEPARTING,
-        sunday=DepartsOnDay.NOT_DEPARTING,
-        start_date=datetime.datetime.now(),
-        end_date=datetime.datetime.now() + datetime.timedelta(days=8),
+        start_date=start_date,
+        end_date=end_date,
     )
-    duplicate_object = Calendar(
+    duplicate_object = calendar_factory(
         service_id="1",
-        monday=DepartsOnDay.NOT_DEPARTING,
-        tuesday=DepartsOnDay.NOT_DEPARTING,
-        wednesday=DepartsOnDay.NOT_DEPARTING,
-        thursday=DepartsOnDay.NOT_DEPARTING,
-        friday=DepartsOnDay.NOT_DEPARTING,
-        saturday=DepartsOnDay.NOT_DEPARTING,
-        sunday=DepartsOnDay.NOT_DEPARTING,
-        start_date=datetime.datetime.now(),
-        end_date=datetime.datetime.now() + datetime.timedelta(days=8),
+        start_date=start_date,
+        end_date=end_date,
     )
     session.add(first_object)
     session.commit()
