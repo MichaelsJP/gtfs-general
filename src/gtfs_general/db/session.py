35b288e75f379c351311f1f56f6c79d1e6bc4915
Julian Psotta <julianpsotta@gmail.com>
2023-12-26 17:11:10 +0100

    fix: add ubuntu spatialite loading fix

--- src/gtfs_general/db/session.py
+++ src/gtfs_general/db/session.py
@@ -5,10 +5,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 from sqlalchemy.event import listen
 
+from gtfs_general import logger
+
 
 def load_spatialite(dbapi_conn: Any, connection_record: Any) -> None:
     dbapi_conn.enable_load_extension(True)
-    dbapi_conn.load_extension("mod_spatialite")
+    try:
+        logger.info("Trying to load mod_spatialite")
+        dbapi_conn.load_extension("mod_spatialite")
+    except Exception as err:
+        logger.warning("Failed to load mod_spatialite: %s", err)
+
+    try:
+        logger.info("Trying to load mod_spatialite.so")
+        dbapi_conn.load_extension("mod_spatialite.so")
+    except Exception as err:
+        logger.error("Failed to load mod_spatialite.so: %s", err)
+        raise err
 
 
 def create_spatialite_db(path: Path, db_name: str = "test.db") -> Engine:
