7759c0f9f2a02c8e90e7a097adb7051d796d712c
Julian Psotta <julianpsotta@gmail.com>
2023-12-26 18:37:59 +0100

    fix: Remove spatialite. Let's do everything with dask!

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
 import pathlib
-import random
 import shutil
-import string
 import zipfile
 from pathlib import Path
 from typing import Generator
@@ -9,11 +7,8 @@ from typing import Generator
 import pytest
 from _pytest.tmpdir import TempPathFactory
 from fastapi.testclient import TestClient
-from sqlalchemy.event import listen
-from sqlmodel import Session, SQLModel, create_engine
 
 from gtfs_general.application import create_app
-from gtfs_general.db.session import create_spatialite_db, load_spatialite
 
 script_path = pathlib.Path(__file__).parent.resolve()
 
@@ -22,32 +17,6 @@ script_path = pathlib.Path(__file__).parent.resolve()
 def test_client() -> Generator[TestClient, None, None]:
     with TestClient(create_app()) as c:
         yield c
-
-
-@pytest.fixture(scope="function")
-def in_memory_spatialite_function_session() -> Generator[Session, None, None]:
-    engine = create_engine("sqlite:///:memory:", echo=True)
-    listen(engine, "connect", load_spatialite)
-    conn = engine.connect()
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
-    SQLModel.metadata.drop_all(engine, checkfirst=True)
-    conn.close()
-
-
-@pytest.fixture(scope="function")
-def in_mapped_spatialite_function_session(tmp_path_factory: TempPathFactory) -> Generator[Session, None, None]:
-    # Create random db name as a string
-    db_name = "".join(random.sample(string.ascii_lowercase, 4)) + ".db"
-    engine = create_spatialite_db(tmp_path_factory.mktemp("db-files"), db_name)
-    listen(engine, "connect", load_spatialite)
-    conn = engine.connect()
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
-    SQLModel.metadata.drop_all(engine, checkfirst=True)
-    conn.close()
 
 
 @pytest.fixture(scope="function")
--- tests/gtfs_general/db/test_session.py
+++
@@ -1,5 +0,0 @@
-from sqlmodel import Session
-
-
-def test_get_session(in_memory_spatialite_function_session: Session) -> None:
-    assert isinstance(in_memory_spatialite_function_session, Session)
