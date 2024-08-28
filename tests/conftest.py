649ef5407c6eb6e2ee24424e397740e5bf56acc9
Julian Psotta <julian.psotta@heigit.org>
2024-08-28 14:29:49 +0200

    tests: Add work in progress

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+import contextlib
+import pathlib
+import shutil
+import socket
+import zipfile
+from pathlib import Path
+from typing import Generator
+
+import pytest
+from _pytest.tmpdir import TempPathFactory
+from fastapi.testclient import TestClient
+
+from gtfs_general.application import create_app
+
+script_path = pathlib.Path(__file__).parent.resolve()
+
+
+@pytest.fixture(scope="module")
+def test_client() -> Generator[TestClient, None, None]:
+    with TestClient(create_app()) as c:
+        yield c
+
+
+# create fixture to return random open port
+@pytest.fixture(scope="function")
+def random_open_port() -> int:
+    with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(("", 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
+
+@pytest.fixture(scope="function")
+def gtfs_test_folder(
+    tmp_path_factory: TempPathFactory,
+) -> Generator[Path, None, None]:
+    tmp_path: Path = tmp_path_factory.mktemp("test_files")
+    test_gtfs_file: str = script_path.joinpath("files/ic_ice_gtfs_germany.zip").__str__()
+    with zipfile.ZipFile(test_gtfs_file, "r") as zip_ref:
+        zip_ref.extractall(tmp_path)
+    yield tmp_path
+    shutil.rmtree(tmp_path)
---
+++ tests/gtfs_general/api/api_v1/test_health.py
@@ -0,0 +1,16 @@
+import pathlib
+
+from fastapi.testclient import TestClient
+from typer.testing import CliRunner
+
+from gtfs_general.config import Settings
+
+runner = CliRunner()
+
+script_path = pathlib.Path(__file__).parent.resolve()
+
+
+def test_server(test_client: TestClient) -> None:
+    response = test_client.get(f"{Settings().api_v1_str}/health")
+    assert response.status_code == 200
+    assert response.json() == ["healthy!"]
